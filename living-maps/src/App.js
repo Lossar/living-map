c52a2d2bb65f1fc5081626235f6eb1e12ed975bd
lossar <larsivarramberg@yahoo.no>
2022-06-27 22:35:14 +0200

    Add basic 3D rendering

--- living-maps/src/App.js
+++ living-maps/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber'
+import {OrbitControls} from '@react-three/drei'
+import Box from './components/Box';
+import BackDrop from './components/Backdrop';
+
+import "./styles/main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <BackDrop/>
+      <ambientLight></ambientLight>
+      <OrbitControls/>
+      <Box></Box>
+    </Canvas>
   );
 }
 
